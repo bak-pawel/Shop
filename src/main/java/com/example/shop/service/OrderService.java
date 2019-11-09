80048318e64569cd34d82ad9037de910392fac58
pawel <pawelbak96@gmail.com>
2019-11-09 12:37:37 +0100

    Required fucntion edit quantity

--- src/main/java/com/example/shop/service/OrderService.java
+++ src/main/java/com/example/shop/service/OrderService.java
@@ -4,6 +4,7 @@ import com.example.shop.model.Drink;
 import com.example.shop.repository.DrinkRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
@@ -19,23 +20,19 @@ public class OrderService {
     public Set<Drink> findDrinkByName(String drinkName) {
        if(drinkName==null|| drinkName.equals("")){
 
-           return (Set<Drink>) drinkRepository.findAll();
+           return new HashSet<>( drinkRepository.findAll());
        }
         return drinkRepository.findDrinkByName(drinkName);
 
     }
     public Set<Drink> findAllDrink(){
-        return (Set<Drink>) drinkRepository.findAll();
+        return new HashSet<>(drinkRepository.findAll());
     }
     public Optional<Drink> findById(int id){
        return drinkRepository.findById(id);
     }
-    public int getQuantity(int id){
-        Optional<Drink> quantity = drinkRepository.findById(id);
-       return quantity.get().getAvailability();
-    }
-    public int setQuantity(int substraction){
-        return drinkRepository.setQuantity(substraction);
-    }
 
+    public void save(Drink drink) {
+        drinkRepository.save(drink);
+    }
 }
